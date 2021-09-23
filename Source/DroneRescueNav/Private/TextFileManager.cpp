b0ac28838507c9b989ba9414b67d018aebf8d4de
Luca <luca@francocovizzi.it>
2021-09-23 18:18:55 +0200

    Add a C++ BP Function Library to save position and orientation
    Currently I'm not able to save all the real time pose but only one

---
+++ Source/DroneRescueNav/Private/TextFileManager.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TextFileManager.h"
+#include "Misc/FileHelper.h"
+#include "HAL/PlatformFilemanager.h"
+
+bool UTextFileManager::SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverWriting = true)
+{
+	//Set complete path
+	SaveDirectory += "\\";
+	SaveDirectory += FileName;
+
+	if (!AllowOverWriting)
+	{
+		if (FPlatformFileManager::Get().GetPlatformFile().FileExists(*SaveDirectory))
+		{
+			return false;
+		}
+	}
+
+	FString FinalString = "";
+
+	for (FString& Each : SaveText)
+	{
+		FinalString += Each;
+		FinalString += LINE_TERMINATOR;
+	}
+
+	return FFileHelper::SaveStringToFile(FinalString, *SaveDirectory);
+}
+
---
+++ Source/DroneRescueNav/Public/TextFileManager.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TextFileManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRONERESCUENAV_API UTextFileManager : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	// Callable from BluePrint
+	UFUNCTION(BlueprintCallable, Category = "Custom", meta = (keywords = "Save"))
+
+	// Function Declataration
+	static bool SaveArrayText(FString SaveDirectory, FString FileName, TArray<FString> SaveText, bool AllowOverWriting); 
+	
+};
